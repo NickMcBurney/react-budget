38457e502410df45e2d94c491100e173d1ebbcd4
Nick McBurney <nick@nickmcburney.co.uk>
2016-09-08 13:55:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import Header from './modules/formLayout';
+//import MortgageCalculator from './modules/MortgageCalculator';
 
+let App = React.createClass({
+    render() {
+        return (
+            <div>
+                <Header title="React ES6 Mortgage Calculator"/>
+                
+            </div>
+        );
+    }
+});
+
+React.render(<App/>, document.body);
